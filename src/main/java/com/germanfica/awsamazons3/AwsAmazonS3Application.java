55ffe01d43ce0ee1d370ff06008dc3dcf17aafd3
German Fica <germafica@gmail.com>
2022-02-05 08:36:12 -0300

    Add maven project

---
+++ src/main/java/com/germanfica/awsamazons3/AwsAmazonS3Application.java
@@ -0,0 +1,13 @@
+package com.germanfica.awsamazons3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAmazonS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsAmazonS3Application.class, args);
+	}
+
+}
